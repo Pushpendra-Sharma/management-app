5ec876792a627be286b8c677c34159c7a98e26eb
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-05-12 01:13:22 +0530

    fix: seperated individual radio components

--- src/components/RadioInput.jsx
+++ src/components/RadioInput.jsx
@@ -1,15 +1,10 @@
-import React, {
-  Fragment,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import { RadioGroup } from '@atlaskit/radio';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { Radio } from '@atlaskit/radio';
 import { AppContext } from '../contexts/app-context';
 import { SET_GENDER_FILTER } from '../actions';
+import '../styles/filter.css';
 
-const RadioInput = ({ options }) => {
+const RadioInput = ({ options, title }) => {
   const [onChangeResult, setOnChangeResult] = useState('');
   const { dispatch } = useContext(AppContext);
 
@@ -22,14 +17,20 @@ const RadioInput = ({ options }) => {
   }, [onChangeResult, dispatch]);
 
   return (
-    <Fragment>
-      <h4 id='radiogroup-label'>Select Gender:</h4>
-      <RadioGroup
-        options={options}
-        onChange={onChange}
-        aria-labelledby='radiogroup-label'
-      />
-    </Fragment>
+    <div className='radio-container'>
+      <h4 className=''>{title}</h4>
+      {options.map(option => (
+        <Radio
+          key={option.value}
+          value={option.value}
+          label={option.label}
+          name={title}
+          testId='radio-default'
+          isChecked={option.value === onChangeResult}
+          onChange={onChange}
+        />
+      ))}
+    </div>
   );
 };
 
