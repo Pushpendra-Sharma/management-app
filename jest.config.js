5972d02fcd0939a1001550472b36530ea596f507
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-05-12 10:56:43 +0530

    chore(tests): configured test dependencies

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/**', '!**/index.js', '!**/mocks/**'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['\\\\node_modules\\\\', 'src/assets'],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 40,
+      lines: 40,
+      functions: 40,
+      statements: 40,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(jpeg|png|webp|svg)': '../mocks/dummyImage.js',
+  },
+  testEnvironment: 'jsdom',
+};
